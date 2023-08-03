cecf38c38f5fa329324463b0a4c4c857a164f113
Martin Bielik <mx.bielik@gmail.com>
2023-08-03 18:41:10 +0200

    inline-wheel-picker release

---
+++ dist/components/SelectionMarker/SelectionMarkerStyles.d.ts
@@ -0,0 +1,24 @@
+declare const _default: {
+    selectionMarkerContainer: {
+        width: string;
+        height: string;
+        position: "absolute";
+        top: number;
+        left: number;
+        flex: number;
+        alignItems: "center";
+        justifyContent: "center";
+        paddingLeft: number;
+        paddingRight: number;
+    };
+    selectionMarkerBorder: {
+        width: string;
+        height: number;
+    };
+    selectionMarker: {
+        width: string;
+        height: number;
+        borderRadius: number;
+    };
+};
+export default _default;
